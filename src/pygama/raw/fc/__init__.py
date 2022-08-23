c56fcbc3dd58cf8e63b13b9b436bde6195e4534d
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-08-23 18:12:09 +0200

    [raw.fc] nicer error message if fcutils is not installed

--- src/pygama/raw/fc/__init__.py
+++ src/pygama/raw/fc/__init__.py
@@ -2,3 +2,17 @@
 This subpackage overloads decoding utilities defined in :mod:`pygama.raw` to
 read files produced by the FlashCam acquisition system.
 """
+
+import logging
+
+log = logging.getLogger(__name__)
+
+try:
+    import fcutils  # noqa: F401
+
+except ModuleNotFoundError as msg:
+    raise ModuleNotFoundError(
+        f"No module named '{msg.name}'. Hint: fcutils is needed for decoding "
+        "FlashCam files. See https://github.com/legend-exp/pyfcutils for "
+        "install instructions."
+    )
